a18f75720b761f31e4bd9eb0fce84c4cb727a4e0
Ivan <ivan5420@yandex.ru>
2021-05-28 10:47:21 +0300

    add section Services

--- app/pages/main.php
+++ app/pages/main.php
@@ -5,8 +5,8 @@ include_once($_SERVER['DOCUMENT_ROOT']. "/app/layouts/header.php");
  ?>
 
 
-<main class="container-fluid  ">
-  <div class="container-fluid main-okna">
+<main class="container-fluid  p-0">
+  <div class="container-fluid main-okna p-0">
     <div class="container main-okna-content ">
       <div class="col-lg-7 h1-block">
         <h1>
@@ -43,16 +43,16 @@ include_once($_SERVER['DOCUMENT_ROOT']. "/app/layouts/header.php");
 
         </div>
         <div class="col-lg-7 second">
-          <h2>
-            <span class="gray first">ОБЪЯВЛЯЕМ ГОД</span>
-            <span class="gray second">ДЕТСКОЙ БЕЗОПАСНОСТИ</span>
+          <h2 class="mb-5">
+            <span class="gray first title-42">ОБЪЯВЛЯЕМ ГОД</span>
+            <span class="gray second title-32">ДЕТСКОЙ&nbsp;БЕЗОПАСНОСТИ</span>
           </h2>
           <div class="d-flex justify-content-between align-items-center">
             <img src="/src/img/ruchka-s-zamkom.png" alt="Ручка с замком в подарок">
             <span class="pink plus">+</span>
-            <h3 class="pink">
+            <h3 class="pink text-32">
               ручка с замком<br />
-              <span>В ПОДАРОК</span><br />
+              <span class="title-32">В ПОДАРОК</span><br />
               к каждому окну
             </h3>
 
@@ -63,5 +63,54 @@ include_once($_SERVER['DOCUMENT_ROOT']. "/app/layouts/header.php");
       </div>
     </div>
   </div>
+  <div class="container p-0 auth-partner pt-100 pb-100 gray">
+    <h2 class="title-42">АВТОРИЗОВАННЫЙ ПАРТНЕР REHAU</h2>
+    <div class="row">
+      <div class="col-lg-7 text-18-32" >
+        <p class="mb-5">Салон энергосберегающих окон – официальный партнер REHAU. Мы поставляем и устанавливаем оконные системы любой сложности. Отлаженное автоматизированное производство пластиковых окон позволяет гарантировать качество на всех этапах изготовления и уменьшать стоимость изделий.</p>
+        <ul>
+          <li>Наш <strong>инженер по замеру бесплатно</strong> посетит ваш объект и предложит оптимальную комплектацию.</li>
+          <li> <strong>Монтаж пластиковых окон</strong>– задача не менее важная, чем выбор профиля и стеклопакета.</li>
+          <li> <strong>Все сотрудники компании регулярно сдают тесты </strong> на подтверждение квалификации, проходят обучение работе с новыми технологиями и применяют только соответствующие нормам материалы и методы работы.</li>
+        </ul>
+      </div>
+      <div class="col-lg-5">
+        <img class="mt-2" src="/src/img/sertificat.jpg" alt="Сертификат официального партнера рехау">
+      </div>
+    </div>
+    <div class="row advant-round-wrapper justify-content-between pt-50 pb-50">
+      <div class="card-item-round text-center">
+        <img src="/src/img/logo.svg" alt="Качество окон">
+        <h3 class="title-18 mt-3">НЕМЕЦКОЕ КАЧЕСТВО REHAU</h3>
+      </div>
+      <div class="card-item-round text-center">
+        <img src="/src/img/garuant.svg" alt="Гарантия на окна">
+        <h3 class="title-18 mt-3">ГАРАНТИЯ 5ЛЕТ</h3>
+      </div>
+      <div class="card-item-round text-center">
+        <img src="/src/img/10-years.svg" alt="Опыт работы">
+        <h3 class="title-18 mt-3">РАБОТАЕМ С 2010 ГОДА</h3>
+      </div>
+      <div class="card-item-round text-center">
+        <img src="/src/img/zamer.svg" alt="Замер окон">
+        <h3 class="title-18 mt-3">БЕСПЛАТНЫЙ ЗАМЕР</h3>
+      </div>
+      <div class="card-item-round text-center">
+        <img src="/src/img/window.svg" alt="Формы окон">
+        <h3 class="title-18 mt-3">ЛЮБЫЕ ФОРМЫ И РАЗМЕРЫ</h3>
+      </div>
+      <div class="card-item-round text-center">
+        <img src="/src/img/low-price.svg" alt="Цены на окна">
+        <h3 class="title-18 mt-3">ДОСТУПНЫЕ ЦЕНЫ</h3>
+      </div>
+    </div>
+  </div>
+  <div class="container services">
+    <div class="row services-logo">
+      <img src="/src/img/salon-logo-text.svg" alt="салон окон рехау">
+    </div>
+    <?php include_once($_SERVER['DOCUMENT_ROOT']. "/app/templates/section/services.php");   ?>
 
+
+  </div>
 </main>
---
+++ app/templates/section/services.php
@@ -0,0 +1,42 @@
+<div class="row wrapper pt-100 justify-content-between">
+  <div class="services-card-okna services-card d-flex justify-content-center align-items-end align-content-end">
+    <div class="d-flex services-white">
+      <a href="#">
+        <span class="d-flex">
+          <img src="/src/img/window-pink.svg" alt="Пластиковые окна">
+          <span class="gray text-uppercase">окна<br />rehau</span>
+        </span>
+      </a>
+    </div>
+  </div>
+  <div class="services-card-balkon services-card d-flex justify-content-center align-items-end align-content-end">
+    <div class="d-flex services-white">
+      <a href="/balkon">
+        <span class="d-flex">
+          <img src="/src/img/balkon-pink.svg" alt="Остекление балконов">
+          <span class="gray text-uppercase">балконы<br />лоджии</span>
+        </span>
+      </a>
+    </div>
+  </div>
+  <div class="services-card-house services-card d-flex justify-content-center align-items-end align-content-end">
+    <div class="d-flex services-white">
+      <a href="/">
+        <span class="d-flex">
+          <img src="/src/img/house-pink.svg" alt="Окна для коттеджей">
+          <span class="gray text-uppercase">Окна<br />в коттедж</span>
+        </span>
+      </a>
+    </div>
+  </div>
+  <div class="services-card-child services-card d-flex justify-content-center align-items-end align-content-end">
+    <div class="d-flex services-white">
+      <a href="/">
+        <span class="d-flex">
+          <img src="/src/img/child-pink.svg" alt="Детские замки на окна">
+          <span class="gray text-uppercase">Детская<br />безопасность</span>
+        </span>
+      </a>
+    </div>
+  </div>
+</div>
