ed4d8af523a24d74b0dc68a5162c6277b7cca786
Ivan <ivan5420@yandex.ru>
2021-06-07 15:18:27 +0300

    add css for deti page

--- app/pages/deti.php
+++ app/pages/deti.php
@@ -13,15 +13,16 @@ include_once($_SERVER['DOCUMENT_ROOT']. "/app/layouts/header.php");
     <div class="container d-flex flex-wrap pt-50 pb-90 deti-main">
       <div class="col-12 col-lg-8 d-flex flex-wrap align-items-center deti-main-text">
         <h1 class="title-32">БЕЗОПАСНОСТЬ ДЕТЕЙ ПОД КОНТРОЛЕМ - СИСТЕМЫ БЕЗОПАСНОСТИ ДЛЯ ОКОН ПВХ</h1>
+        <img class="img-fluid d-block d-lg-none pt-30 pb-30" src="/src/img/deti01.jpg" alt="Детские замки на окна">
         <p class="text-18">Пытливый ум не дает маленьким сорванцам усидеть на месте, а окно, является очень притягательным объектом. Не зря оно признается экспертами одним из самых опасных объектов для ребенка в доме. </p>
         <p class="text-18">Различные конструкции детских замков могут обеспечить как банальную невозможность открыть створку без ключа, так и ограничение на открывание у продвинутых моделей, для сохранения возможности проветривания помещения в Ваше отсутствие.</p>
       </div>
-      <div class="col-12 col-lg-4">
+      <div class="col-12 col-lg-4 d-none d-lg-block">
         <img class="img-fluid" src="/src/img/deti01.jpg" alt="Детские замки на окна">
       </div>
     </div>
   </div>
-  <div class="container text pt-90 pb-90">
+  <div class="container text pt-90 pb-90 deti-green">
     <div class="bg-green p-5">
       <h2 class="white text-28">ВАЖНО! Нормативное регулирование!</h2>
       <p class="m-0 white text-20">Согласно ПРИКАЗУ от 17 марта 2016 года N 168-ст «О введении в действие изменения к межгосударственному стандарту» введены Изменения N 1 к ГОСТ 23166-99 «Блоки оконные. Общие технические условия». Данными изменениями предписывается обязательная установка на пластиковых конструкциях детских замков. Предписание касается, прежде всего, бюджетных учреждений, таких как больницы, школы, детские сады и прочие, деятельность которых связана с детьми и напрямую не обязывает частных домовладельцев устанавливать замки безопасности. Однако, если в результате невыполнения рекомендаций, к слову, внесенных в ГОСТ с подачи Следственного Комитета РФ, пострадает ребенок — владелец помещения может быть привлечён к уголовной ответственности.</p>
@@ -87,17 +88,17 @@ include_once($_SERVER['DOCUMENT_ROOT']. "/app/layouts/header.php");
           <p>Установка специализированной защитной ручки не требует серьезного вмешательства в оконную конструкцию, так как новая ручка будет установлена на место старой. Внешний вид окна при этом ничуть не испортится.</p>
           <p>Подвидом такого устройства является Ручка с кнопкой. В таком варианте исполнения запор с ключом заменяется на кнопку с большим усилием нажатия (нажать подобную кнопку не в силах даже крепкий ребенок. Только взрослый). Такое решение позволяет устранить неудобства с постоянным использованием ключа, как отдельного объекта, который нужно держать вне доступа ребенка.</p>
         </div>
-        <div class="deti-img d-flex">
-          <div class="col-3 p-3">
+        <div class="deti-img d-flex flex-wrap">
+          <div class="col-6 col-lg-3 p-3">
             <img class=img-fluid src="/src/img/deti-1-1.jpg" alt="Ручка с замком">
           </div>
-          <div class="col-3 p-3">
+          <div class="col-6 col-lg-3 p-3">
             <img class=img-fluid src="/src/img/deti1-2.jpg" alt="Ручка с замком">
           </div>
-          <div class="col-3 p-3">
+          <div class="col-6 col-lg-3 p-3">
             <img class=img-fluid src="/src/img/deti1-3.jpg" alt="Ручка с замком">
           </div>
-          <div class="col-3 p-3 d-flex justify-content-center align-items-center">
+          <div class="col-6 col-lg-3 p-3 d-flex justify-content-center align-items-center">
             <button class="white " type="button" name="button-deti" onclick="modalVisible()">Заказать</button>
           </div>
         </div>
@@ -108,17 +109,17 @@ include_once($_SERVER['DOCUMENT_ROOT']. "/app/layouts/header.php");
           <h3>Накладной замок</h3>
           <p>Данный тип также состоит из двух элементов, крепящихся на створке и раме соответственно. Отличительной особенностью является возможность без отпирания запорного механизма откинуть створку для проветривания. Для этого такой замок крепится в нижней части окна. Однако, никто не запретит поставить его на любую из сторон окна, если Вам этого захочется.</p>
         </div>
-        <div class="deti-img d-flex">
-          <div class="col-3 p-3">
+        <div class="deti-img d-flex flex-wrap">
+          <div class="col-6 col-lg-3 p-3">
             <img class=img-fluid src="/src/img/deti2-1.jpg" alt="Накладной замок на окна">
           </div>
-          <div class="col-3 p-3">
+          <div class="col-6 col-lg-3 p-3">
             <img class=img-fluid src="/src/img/deti2-2.jpg" alt="Накладной замок на окна">
           </div>
-          <div class="col-3 p-3">
+          <div class="col-6 col-lg-3 p-3">
             <img class=img-fluid src="/src/img/deti2-3.jpg" alt="Накладной замок на окна">
           </div>
-          <div class="col-3 p-3 d-flex justify-content-center align-items-center">
+          <div class="col-6 col-lg-3 p-3 d-flex justify-content-center align-items-center">
             <button class="white " type="button" name="button-deti" onclick="modalVisible()">Заказать</button>
           </div>
         </div>
@@ -130,17 +131,17 @@ include_once($_SERVER['DOCUMENT_ROOT']. "/app/layouts/header.php");
           <p>Принцип действия схож с замками для велосипедов. Одна из частей конструкции данного замка крепится к раме, другая — к створке. Трос, крепящийся на одной из частей фиксируется в пазах другой части при помощи ключа. Длину тросика можно выбрать в зависимости от модели. Также такие замки могут отличаться возможностью избавляться от троса полностью.</p>
           <p>Тросовые замки снискали наибольшую популярность для раздвижных оконных конструкций.</p>
         </div>
-        <div class="deti-img d-flex">
-          <div class="col-3 p-3">
+        <div class="deti-img d-flex flex-wrap">
+          <div class="col-6 col-lg-3 p-3">
             <img class=img-fluid src="/src/img/deti3-1.jpg" alt="Замок с тросиком на окна">
           </div>
-          <div class="col-3 p-3">
+          <div class="col-6 col-lg-3 p-3">
             <img class=img-fluid src="/src/img/deti3-2.jpg" alt="Замок с тросиком на окна">
           </div>
-          <div class="col-3 p-3">
+          <div class="col-6 col-lg-3 p-3">
             <img class=img-fluid src="/src/img/deti3-3.jpg" alt="Замок с тросиком на окна">
           </div>
-          <div class="col-3 p-3 d-flex justify-content-center align-items-center">
+          <div class="col-6 col-lg-3 p-3 d-flex justify-content-center align-items-center">
             <button class="white " type="button" name="button-deti" onclick="modalVisible()">Заказать</button>
           </div>
         </div>
@@ -151,17 +152,17 @@ include_once($_SERVER['DOCUMENT_ROOT']. "/app/layouts/header.php");
           <h3>Фиксатор</h3>
           <p>Блокиратор или же защелка представляет собой конструкцию, в которой блокировка распахивания окна осуществляется поворотом подвижных элементов механизма. Такой замок можно установить в верхней части окна, до которой дети просто не дотянутся. Или же установить несколько таких устройств. Отрицательным моментом будет являться внешний вид. Не каждому нравится обилие чужеродных элементов на окнах.</p>
         </div>
-        <div class="deti-img d-flex">
-          <div class="col-3 p-3">
+        <div class="deti-img d-flex flex-wrap">
+          <div class="col-6 col-lg-3 p-3">
             <img class=img-fluid src="/src/img/deti4-1.jpg" alt="Фиксатор на окна">
           </div>
-          <div class="col-3 p-3">
+          <div class="col-6 col-lg-3 p-3">
             <img class=img-fluid src="/src/img/deti4-2.jpg" alt="Фиксатор на окна">
           </div>
-          <div class="col-3 p-3">
+          <div class="col-6 col-lg-3 p-3">
             <img class=img-fluid src="/src/img/deti4-3.jpg" alt="Фиксатор на окна">
           </div>
-          <div class="col-3 p-3 d-flex justify-content-center align-items-center">
+          <div class="col-6 col-lg-3 p-3 d-flex justify-content-center align-items-center">
             <button class="white " type="button" name="button-deti" onclick="modalVisible()">Заказать</button>
           </div>
         </div>
@@ -169,14 +170,15 @@ include_once($_SERVER['DOCUMENT_ROOT']. "/app/layouts/header.php");
 
     </div>
 
-    <div class="container mt-70 mb-70 p-5 d-flex babysafe" id="deti-5">
-      <div class="col-lg-9 babysafe-text">
+    <div class="container mt-70 mb-70 p-5 flex-wrap d-flex babysafe" id="deti-5">
+      <div class="col-12 col-lg-9 babysafe-text">
         <div class="white title-32">ЭКСКЛЮЗИВНО!<br />ТОЛЬКО В САЛОНЕ REHAU -</div>
         <h3 class="white title-28">РЕШЕТКА BABYSAFE</h3>
+        <img class="img-fluid d-block d-lg-none mb-30" src="/src/img/babysafe01.jpg" alt="решетки на окна babysafe">
         <p>Защитное ограждение REHAU BABYSAFE устанавливается снаружи окна и позволяет открывать створку во всех режимах проветривания, в том числе нараспах, с уверенностью в безопасности маленьких детей. </p>
         <p>Данное ограждение сочетает в себе надежность конструкции, простой и легкий монтаж, а также привлекательный внешний вид.</p>
       </div>
-      <div class="col-lg-3">
+      <div class="col-12 col-lg-3 d-none d-lg-block">
         <img class="img-fluid" src="/src/img/babysafe01.jpg" alt="решетки на окна babysafe">
       </div>
     </div>
